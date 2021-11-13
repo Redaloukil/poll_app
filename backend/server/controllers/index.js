07fa699bd2f63fa3e3ec9d254aaaef9c22333449
Reda <loukil.red@gmail.com>
2021-11-13 21:24:18 +0100

    remove all unused files

--- backend/server/controllers/index.js
+++ backend/server/controllers/index.js
@@ -1,11 +1,7 @@
 const userController = require('./users');
 const pollsController = require('./polls');
-const choicesController = require('./choices');
-const votesController = require('./votes');
 
 module.exports = {
   userController,
-  pollsController,
-  choicesController,
-  votesController
+  pollsController
 };
